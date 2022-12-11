3425c2858e7f4d63b4e4997c69e0ff62cab69ff4
wgmin <min20000416@naver.com>
2022-12-11 22:45:28 +0900

    [22.12.11] 회원가입 수정

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,7 @@ class AccountsUser(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="accounts_user", on_delete=models.CASCADE)
     name = models.CharField(max_length=16, null=False, blank=False, verbose_name="이름")
     role = models.CharField(max_length=16, null=False, blank=False, default="user", verbose_name="역할")
-    birth = models.DateTimeField(null=False, blank=False, verbose_name="생년월일")
+    birth = models.DateField(null=False, blank=False, verbose_name="생년월일")
     phone = models.CharField(max_length=11, null=False, blank=False, verbose_name="전화번호")
     address = models.CharField(max_length=128, null=True, blank=True, verbose_name="주소")
     sleep_yn = models.BooleanField(null=False, blank=False, default=False, verbose_name="장기불참 여부")
--- accounts/views.py
+++ accounts/views.py
@@ -1,27 +1,28 @@
 from .models import AccountsUser
 from django.contrib import auth
-from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+from django.db import transaction
 
+@transaction.atomic
 def sign_up(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
             # auth_user save
             user = User.objects.create_user(
-                username=request.POST['username'],
-                password=request.POST['password1'],
-                email=request.POST['email']
+                username=request.POST.get('username'),
+                password=request.POST.get('password1'),
+                email=request.POST.get('email')
             )
             print(user)
-            # user_id = getattr(User.objects.get(id=user.id))
+            user_id = getattr(User.objects.get(id=user.id), "id")
             # accounts_user save
             accounts_user = AccountsUser.objects.create(
-                name=request.POST['name'],
-                birth=request.POST['birth'],
-                phone=request.POST['phone'],
-                address=request.POST['address'],
-                user_id=1
+                name=request.POST.get('name'),
+                birth=request.POST.get('birth'),
+                phone=request.POST.get('phone'),
+                address=request.POST.get('address'),
+                user_id=user_id
             )
             auth.login(request, user)
             return redirect('/')
